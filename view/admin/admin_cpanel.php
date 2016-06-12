5ab2aa1f385acf3f4a39afd9d37acb9911bdeaeb
Cuong Manh <Cuong Manh>
2016-06-12 21:28:46 +0700

    first commit

---
+++ view/admin/admin_cpanel.php
@@ -0,0 +1,8 @@
+<?php include "../view/admin/header_cpanel.php";?>
+ <div class="main_content">
+    	<div class="slides">
+             <img src="../controller/images/admin_shows.jpg" width="890" height="300" alt="admin_shows" />
+        </div><!--End #slide-->
+        <div class="clear"></div>
+    </div><!--End #main_content-->
+   <?php include "../view/admin/footer.php";?>
---
+++ view/website/about.php
@@ -0,0 +1,25 @@
+<?php include "../view/website/header.php";?>
+ <div class="main_content">
+    	<div class="showtext">
+    <center><h1>Giới thiệu</h1></center>
+    <p>CameraShop Online trực thuộc Công ty TNHH AZ Computer, thành lập vào tháng 05/2013, lĩnh vực hoạt động chính của công ty bao gồm: mua bán sửa chữa các thiết bị liên quan đến trang thiết bị ngành nhiếp ảnh, thiết bị kỹ thuật số.
+    </p>
+    <p><b>1. Quá trình hình thành:</b></p>
+    <p>CameraShop Online trực thuộc Công ty TNHH AZ Computer, thành lập vào tháng 05/2013 bởi thành viên Phạm Trương Ngọc Huy, lĩnh vực hoạt động chính của công ty bao gồm: mua bán sửa chữa các thiết bị liên quan đến trang thiết bị ngành nhiếp ảnh, thiết bị kỹ thuật số.
+    </p>
+    <p>
+    Bằng trải nghiệm về thị trường KTS và điện tử trong nhiều năm, cùng với việc nghiên cứu kỹ tập quán mua hàng của khách hàng Việt Nam, CameraShop Online đã xây dựng một phương thức kinh doanh chưa từng có ở Việt Nam trước đây. Công ty đã xây dựng được một phong cách tư vấn bán hàng đặc biệt nhờ vào một đội ngũ nhân viên chuyên nghiệp và trang web www.cameraShopOnline.com hỗ trợ như là một cẩm nang về trang thiết bị ngành nhiếp ảnh, thiết bị kỹ thuật số hàng đầu tại Việt Nam.
+  </p>
+    <p>
+    Hiện nay, số lượng sản phẩm bán ra trung bình tại www.cameraShopOnline.com khoảng 1.000 máy/tháng chiếm khoảng 15% thị phần thiết bị KTS của cả nước. Trung bình một tháng bán ra hơn 700 thiết bị và phụ kiện trở thành một trong những Nhà bán lẻ trang thiết bị KTS  ngành ảnh với doanh số hàng đầu cả nước.
+    </p>
+    <p><b>2. Hệ thống chi nhánh:</b></p>
+     <p>Chi nhánh HCM: 391A Nam Kỳ Khởi Nghĩa, Quận 3, TP. Hồ Chí Minh   ĐT :(08) 3 526 8799 – (08) 3 526 8896</p>
+     <p>Chi nhánh Tây Nguyên : 164 Phan Chu Trinh, TP. Tam Kỳ ,Tỉnh Quảng Nam  ĐT : (0500) 355 5678 – Fax: (0500) 355 6996</p>
+     <p>Chi nhánh Đà Nẵng :  137 Nguyễn Thị Thập, Phường Hòa Minh, Quận Liên Chiểu, Đà Nẵng.  ĐT :(0511) 371 0999 – 0919 708571</p>
+     <br/><br/>
+     <center><p><img src="../controller/images/admin_shows.jpg" width="700" height="300" alt="about" /></p></center>
+    </div>
+        <div class="clear"></div>
+    </div><!--End #main_content-->
+<?php include "../view/website/footer.php";?>
